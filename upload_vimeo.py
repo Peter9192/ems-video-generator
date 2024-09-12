884fef2784584d3b5b7bfdb76d18b52c3070c355
Peter Kalverla <peter.kalverla@gmx.com>
2024-09-12 16:59:38 +0200

    Successfully interact with vimeo API

---
+++ upload_vimeo.py
@@ -0,0 +1,32 @@
+# Script to upload video to Vimeo using the API
+#
+# Follows steps from https://developer.vimeo.com/api/guides/start
+# and https://developer.vimeo.com/api/guides/videos/upload
+#
+# Requires one to register an "app" in vimeo account.
+# The app will have a client_id, client_secret, and you need to add a personal access token with upload and edit scopes
+# These should be stored in a file called `credentials_vimeo.toml` as such:
+#
+# client_id = "<your client id>"
+# client_secret = "<your client secret>"
+# access_token = "<your personal access token>"
+#
+# This script uses the Vimeo python client, which can be installed with
+#
+#   pip install PyVimeo
+#
+
+import tomllib
+import vimeo
+
+with open("credentials_vimeo.toml", "rb") as f:
+    secrets = tomllib.load(f)
+
+client = vimeo.VimeoClient(
+    token=secrets["access_token"],
+    key=secrets["client_id"],
+    secret=secrets["client_secret"],
+)
+
+response = client.get("/tutorial")
+print(response.json())
