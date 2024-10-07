1c40538ea182c2c19a200188e72990488de3b23c
Peter Kalverla <peter.kalverla@gmx.com>
2024-10-07 19:16:56 +0200

    try interacting with existing upload

--- upload_vimeo.py
+++ upload_vimeo.py
@@ -17,6 +17,7 @@
 #
 
 import tomllib
+from requests import HTTPError
 import vimeo
 
 with open("credentials_vimeo.toml", "rb") as f:
@@ -28,13 +29,43 @@ client = vimeo.VimeoClient(
     secret=secrets["client_secret"],
 )
 
-file_name = "ems_intro.mp4"
-uri = client.upload(
-    file_name,
-    data={
-        "name": "Test upload via API",
-        "description": "Trying to upload the EMS intro using the vimeo API",
-    },
-)
+# file_name = "ems_intro.mp4"
+# uri = client.upload(
+#     file_name,
+#     data={
+#         "name": "Test upload via API",
+#         "description": "Trying to upload the EMS intro using the vimeo API",
+#     },
+# )
 
-print(f"Your video URI is: {uri}")
+# print(f"Your video URI is: {uri}")
+
+uri = "/videos/1008853136"
+
+# Check status
+response = client.get(uri + "?fields=transcode.status").json()
+if response["transcode"]["status"] == "complete":
+    print("Your video finished transcoding.")
+elif response["transcode"]["status"] == "in_progress":
+    print("Your video is still transcoding.")
+else:
+    print("Your video encountered an error during transcoding.")
+
+# Update description (post-upload)
+response = client.patch(uri, data={"description": "test editing via api"})
+response.raise_for_status()
+
+# Set a password (post-upload)
+response = client.patch(
+    uri, data={"privacy": {"view": "password"}, "password": "helloworld"}
+)
+try:
+    response.raise_for_status()
+except HTTPError as exc:
+    info = response.json()
+    raise HTTPError(f"Request failed with following info: {info}") from exc
+
+# Get link to video
+response = client.get(uri + "?fields=link")
+response.raise_for_status()
+print(f"Your video link is: {response.json()['link']}")
